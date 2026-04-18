af0f6623f4c5b8a9e3c0504c17a5f16f0952923a
CharlesGit30 <charlesgit30@gmail.com>
2026-04-17 21:41:43 -0300

    Add files via upload

--- componentes/header.php
+++ componentes/header.php
@@ -1,1 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header -> BioTech</title>
+    <link rel="stylesheet" href="style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
 
+</head>
+<body>
+    <header>
+        <div class="logo">
+            <img src="" alt="Logo da BioTech">
+        </div>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Sobre Nós</a></li>
+                <li><a href="#">Produtos</a></li>
+                <li><a href="#">Contato</a></li>
+            </ul>
+
+            <div class="header-icons">
+                <a href=""><i class='bx bx-user'></i></a>
+            </div>
+            
+        </nav>
+    </header>
+
+
+    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+</body>
+</html>
