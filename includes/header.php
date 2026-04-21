ee6b236592e30cf6214672115b7661f1a157ebfc
CharlesGit30 <charlesgit30@gmail.com>
2026-04-21 17:20:15 -0300

    Add files via upload

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,40 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header -> Biotech</title>
+    <link rel="stylesheet" href="style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 
+</head>
+<body>
+    
+    <header>
+        <div class="logo">
+            <img src="" alt="Logo da biotech">
+        </div>
+
+        <div class="navbar">
+            <ul>
+                <li><a href="">DashBoard</a>
+                    <a href="">Receita</a>
+                    <a href="">Medicamentos</a>
+                    <a href="">Relatorios</a>
+                </li>
+            </ul>
+        </div>
+
+        <div class="header-icons">
+            <a href=""><i class='bx bx-user'></i></a>
+            <a href=""><i class='bx bx-log-out'></i></a>
+        </div>
+        
+    </header>
+
+    <script src="https://unpkg.com/boxicons@2.1.4/dist/boxicons.js"></script>
+</body>
+</html>
