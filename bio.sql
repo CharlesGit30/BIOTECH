82787eb77b6beca10aa9f1cba8b6da022ca00c8e
CharlesGit30 <charlesgit30@gmail.com>
2026-04-24 21:53:18 -0300

    Add files via upload

--- bio.sql
+++ bio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 25/04/2026 às 02:11
+-- Tempo de geração: 25/04/2026 às 02:52
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
