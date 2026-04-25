de873072dc84424bd84ec49d307d04fcf7ecc2f1
CharlesGit30 <charlesgit30@gmail.com>
2026-04-24 21:52:15 -0300

    Add files via upload

--- processamento/criar.php
+++ processamento/criar.php
@@ -1,1 +1,39 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+</head>
+<body>
+    
 
+<?php 
+    session_start();
+    include "../config.php";
+
+    $nome = $_POST['nome'];
+    $fabricante = $_POST['fabricante'];
+    $tipo = $_POST['tipo'];
+    $quantidade = $_POST['quantidade'];
+    $validade = $_POST['validade'];
+    $composicao = $_POST['composicao'];
+    $data_registro = $_POST['data_registro'];
+
+    $instrucao = $conexao->prepare("INSERT INTO medicamentos (nome, fabricante, tipo, quantidade, validade, composicao, data_registro) VALUES (?, ?, ?, ?, ?, ?, ?)");    
+    $instrucao->bind_param("ssssssss", $nome, $fabricante, $tipo, $quantidade, $validade, $composicao, $data_registro);
+    $instrucao->execute();
+
+    if($instrucao->num_rows > 0){
+        $_SESSION['mensagem'] = "Medicamento cadastrado com sucesso!";
+        header("Location: ../dashboard.php");
+    } else {
+        $_SESSION['mensagem'] = "Erro ao cadastrar medicamento.";
+        header("Location: ../dashboard.php");
+    }
+    $instrucao->close();
+    $conexao->close();
+?>
+
+</body>
+</html>
