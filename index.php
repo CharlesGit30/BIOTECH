4811fc9acb8cb055beb0cf47b03c6042964e7f0c
CharlesGit30 <charlesgit30@gmail.com>
2026-04-27 18:04:27 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BioTech</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+</head>
+<body>
+    <?php include "../includes/header.php" ?>
+
+    <div class="inicio">
+        <div class="inicio-text">
+            <h2></h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestiae dolores corporis, praesentium accusantium temporibus doloremque odio libero! Ipsa quis beatae minima maxime expedita vero commodi ab quasi odio, rerum quia.
+            Vel voluptas alias fuga perferendis dicta tempora itaque! Quam ipsum, velit id voluptatum laudantium magnam labore accusamus sequi optio nisi rerum repudiandae eaque quaerat iste repellendus ratione architecto. Repudiandae, veniam!</p>
+        </div>
+        <div class="inicio-img">
+            <img src="" alt="">
+        </div>
+    </div>
+
+    <div class="logos">
+        <div class="logo"><img src="" alt=""></div>
+        <div class="logo"><img src="" alt=""></div>
+        <div class="logo"><img src="" alt=""></div>
+        <div class="logo"><img src="" alt=""></div>
+        <div class="logo"><img src="" alt=""></div>
+    </div>
+
+    <div class="container">
+        <div class="criar-laudo"></div>
+        <div class="listar-laudo"></div>
+    </div>
+
+    <div class="informacoes">
+
+    </div>
+
+    <?php include "../includes/footer.php"; ?>
+</body>
+</html>
